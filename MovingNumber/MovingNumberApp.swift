a00b6d0eac6b8d660655b2c700eafbdcadb7e7d8
Baranov Roman <palamarmle@gmail.com>
2023-06-19 19:30:25 +0300

    Initial Commit

---
+++ MovingNumber/MovingNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovingNumberApp.swift
+//  MovingNumber
+//
+//  Created by Рома Баранов on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovingNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
